052448a7ea31df1f3b6c53aae456a3eef270c98e
Ofosu Emmanuel <64600434+Quameofosu@users.noreply.github.com>
2024-09-08 04:39:53 +0000

    useEffect practice

--- first-project/src/App.jsx
+++ first-project/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import User from "./User.jsx";
 import './App.css'
+import Product from './Product.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -30,6 +31,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <User/>
+      <Product/>
     </>
     
   )
